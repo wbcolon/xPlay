47b61dfdaf0445fffe7770fbbaa0fa7b2163874b
Werner Backes <mail@wernerbackes.de>
2020-10-20 22:27:50 +0200

    Remove hard coded music library path.

--- xPlay.cpp
+++ xPlay.cpp
@@ -8,7 +8,5 @@ int main(int argc, char* argv[])
     QApplication playApp(argc, argv);
     xApplication app;
     app.show();
-    app.setMusicLibraryDirectory("/samba/media.1/music/");
-
     return playApp.exec();
 }
