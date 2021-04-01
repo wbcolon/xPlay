27dd790561ed9bc88b8498e61476560d35887589
Werner Backes <mail@wernerbackes.de>
2021-04-01 17:34:15 +0200

    Add wrapper class around music files and the taglib scanning.

---
+++ xMusicFile.cpp
@@ -0,0 +1,128 @@
+/*
+ * This file is part of xPlay.
+ *
+ * xPlay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "xMusicFile.h"
+
+#include <QDebug>
+
+#include <taglib/fileref.h>
+#include <taglib/audioproperties.h>
+#include <taglib/flacproperties.h>
+#include <taglib/wavpackproperties.h>
+
+xMusicFile::xMusicFile():
+        QObject(),
+        filePath(),
+        fileArtist(),
+        fileAlbum(),
+        fileTrackName(),
+        fileLength(-1),
+        fileBitsPerSample(-1),
+        fileBitrate(-1),
+        fileSampleRate(-1) {
+}
+
+xMusicFile::xMusicFile(const std::filesystem::path& file, const QString& artist, const QString& album,
+                       const QString& trackName, QObject* parent):
+        QObject(parent),
+        filePath(file),
+        fileArtist(artist),
+        fileAlbum(album),
+        fileTrackName(trackName),
+        fileLength(-1),
+        fileBitsPerSample(-1),
+        fileBitrate(-1),
+        fileSampleRate(-1) {
+}
+
+xMusicFile::xMusicFile(const xMusicFile& file):
+        QObject(file.parent()),
+        filePath(file.filePath),
+        fileArtist(file.fileArtist),
+        fileAlbum(file.fileArtist),
+        fileTrackName(file.fileTrackName),
+        fileLength(file.fileLength),
+        fileBitsPerSample(file.fileBitsPerSample),
+        fileBitrate(file.fileBitrate),
+        fileSampleRate(file.fileSampleRate) {
+}
+
+const std::filesystem::path& xMusicFile::getFilePath() const {
+    return filePath;
+}
+
+const QString& xMusicFile::getArtist() const {
+    return fileArtist;
+}
+
+const QString& xMusicFile::getAlbum() const {
+    return fileAlbum;
+}
+
+const QString& xMusicFile::getTrackName() const {
+    return fileTrackName;
+}
+
+qint64 xMusicFile::getLength() const {
+    scan();
+    return fileLength;
+}
+
+int xMusicFile::getBitsPerSample() const {
+    scan();
+    return fileBitsPerSample;
+}
+
+int xMusicFile::getSampleRate() const {
+    scan();
+    return fileSampleRate;
+}
+
+int xMusicFile::getBitrate() const {
+    scan();
+    return fileBitrate;
+}
+
+bool xMusicFile::isScanned() const {
+    return (fileLength > 0);
+}
+
+void xMusicFile::scan() const {
+    // Check if we have a file defined.
+    if (isScanned() || (filePath.empty())) {
+        return;
+    }
+    // Use taglib to determine the sample rate, bitrate, bits per sample and length.
+    TagLib::FileRef currentTrack(filePath.c_str(), true, TagLib::AudioProperties::Fast);
+    TagLib::AudioProperties* currentTrackProperties = currentTrack.audioProperties();
+    // Most files do only support 16 bits per sample.
+    fileBitsPerSample = 16;
+    try {
+        auto filePathExt = QString::fromStdString(filePath.extension());
+        if (filePathExt.compare(".flac", Qt::CaseInsensitive) == 0) {
+            auto* currentFlacProperties = dynamic_cast<TagLib::FLAC::Properties*>(currentTrackProperties);
+            fileBitsPerSample = currentFlacProperties->bitsPerSample();
+        } else if (filePathExt.compare(".wv", Qt::CaseInsensitive) == 0) {
+            auto* currentWvProperties = dynamic_cast<TagLib::WavPack::Properties*>(currentTrackProperties);
+            fileBitsPerSample = currentWvProperties->bitsPerSample();
+        }
+    } catch(std::bad_cast& e) {
+        // Ignore error.
+        qCritical() << "xMusicFile: unable to scan properties for: "
+                    << QString::fromStdString(filePath.generic_string()) << ", error: " << e.what();
+    }
+    fileBitrate = currentTrackProperties->bitrate();
+    fileSampleRate = currentTrackProperties->sampleRate();
+    fileLength = currentTrackProperties->lengthInMilliseconds();
+}
---
+++ xMusicFile.h
@@ -0,0 +1,117 @@
+/*
+ * This file is part of xPlay.
+ *
+ * xPlay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __XMUSICFILE_H__
+#define __XMUSICFILE_H__
+
+#include <filesystem>
+#include <QObject>
+#include <QString>
+
+class xMusicFile:public QObject {
+public:
+    xMusicFile();
+    xMusicFile(const std::filesystem::path& file, const QString& artist, const QString& album,
+               const QString& trackName, QObject* parent=nullptr);
+    xMusicFile(const xMusicFile& file);
+    ~xMusicFile() override = default;
+    /**
+     * Initiate a onetime scan of the music file.
+     *
+     * Use taglib to determine the length, bitrate, bits per sample,
+     * and the sample rate for the music file.
+     */
+    void scan() const;
+    /**
+     * Determine whether or not the music file was already scanned.
+     *
+     * @return true if the file was scanned, false otherwise.
+     */
+    [[nodiscard]] bool isScanned() const;
+    /**
+     * Retrieve the absolute path to the music file.
+     *
+     * @return the absolute path as std::filesystem::path object.
+     */
+    [[nodiscard]] const std::filesystem::path& getFilePath() const;
+    /**
+     * Retrieve the artist associated with the music file.
+     *
+     * @return the artist as string.
+     */
+    [[nodiscard]] const QString& getArtist() const;
+    /**
+     * Retrieve the album associated with the music file.
+     *
+     * @return the album as string.
+     */
+    [[nodiscard]] const QString& getAlbum() const;
+    /**
+     * Retrieve the track name associated with the music file.
+     *
+     * @return the track name as string.
+     */
+    [[nodiscard]] const QString& getTrackName() const;
+    /**
+     * Retrieve the length of the music file.
+     *
+     * The length is determined by taglib and requires an
+     * additional scan of the music file.
+     *
+     * @return the length in ms as integer.
+     */
+    [[nodiscard]] qint64 getLength() const;
+    /**
+     * Retrieve the bits per sample for the music file.
+     *
+     * The bits per sample are determined by taglib and
+     * require an additional scan of the music file.
+     *
+     * @return the bits per samples as integer.
+     */
+    [[nodiscard]] int getBitsPerSample() const;
+    /**
+     * Retrieve the sample rate for the music file.
+     *
+     * The sample rate is determined by taglib and
+     * requires an additional scan of the music file.
+     *
+     * @return the sample rate as integer
+     */
+    [[nodiscard]] int getSampleRate() const;
+    /**
+     * Retrieve the bitrate for the music file.
+     *
+     * The bitrate is determined by taglib and
+     * requires an additional scan of the music file.
+     *
+     * @return the bitrate as integer.
+     */
+    [[nodiscard]] int getBitrate() const;
+
+private:
+    std::filesystem::path filePath;
+    QString fileArtist;
+    QString fileAlbum;
+    QString fileTrackName;
+    // make file properties mutable, because we scan the file only on demand.
+    mutable qint64 fileLength;
+    mutable int fileBitsPerSample;
+    mutable int fileBitrate;
+    mutable int fileSampleRate;
+};
+
+Q_DECLARE_METATYPE(xMusicFile)
+
+#endif
