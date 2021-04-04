68a6517da6f3506381a4b552ae9986552abc4558
Werner Backes <mail@wernerbackes.de>
2021-04-04 22:15:24 +0200

    Add search widget.

---
+++ xPlayerMusicSearchWidget.cpp
@@ -0,0 +1,97 @@
+/*
+ * This file is part of xPlay.
+ *
+ * xPlay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "xPlayerMusicSearchWidget.h"
+#include "xPlayerUI.h"
+
+#include <QApplication>
+#include <QRadioButton>
+
+xPlayerMusicSearchWidget::xPlayerMusicSearchWidget(QWidget* parent, Qt::WindowFlags flags):
+        QWidget(parent, flags),
+        currentSearchText(),
+        currentSearchCategory(-1) {
+    auto layout = new xPlayerLayout();
+    layout->setSpacing(0);
+    layout->setContentsMargins(0, 0, 0, 0);
+    searchArtistButton = new QRadioButton(tr("Artist"), this);
+    searchArtistButton->setChecked(true);
+    searchAlbumButton = new QRadioButton(tr("Album"), this);
+    searchTrackButton = new QRadioButton(tr("Track"), this);
+    searchCategoryGroup = new QButtonGroup(this);
+    searchCategoryGroup->addButton(searchArtistButton, 1);
+    searchCategoryGroup->addButton(searchAlbumButton, 2);
+    searchCategoryGroup->addButton(searchTrackButton, 3);
+    searchCategoryGroup->setExclusive(true);
+    searchButton = new QPushButton(tr("Search"), this);
+    clearButton = new QPushButton(tr("Clear"), this);
+    searchText = new QLineEdit(this);
+    layout->addWidget(searchText, 0, 0, 1, 6);
+    layout->addColumnSpacer(6, xPlayerLayout::HugeSpace);
+    layout->addWidget(searchArtistButton, 0, 7);
+    layout->addWidget(searchAlbumButton, 0, 8);
+    layout->addWidget(searchTrackButton, 0, 9);
+    layout->addColumnSpacer(10, xPlayerLayout::HugeSpace);
+    layout->addWidget(searchButton, 0, 11);
+    layout->addColumnSpacer(12, xPlayerLayout::SmallSpace);
+    layout->addWidget(clearButton, 0, 13);
+    layout->addColumnSpacer(14, xPlayerLayout::SmallSpace);
+    setLayout(layout);
+    setFixedHeight(static_cast<int>(QFontMetrics(QApplication::font()).height()*xPlayerSelectorHeightFontFactor));
+    connect(searchButton, &QPushButton::pressed, this, &xPlayerMusicSearchWidget::searchClicked);
+    connect(clearButton, &QPushButton::pressed, this, &xPlayerMusicSearchWidget::clearClicked);
+    connect(searchText, &QLineEdit::returnPressed, this, &xPlayerMusicSearchWidget::returnPressed);
+}
+
+std::tuple<QString,QString,QString> xPlayerMusicSearchWidget::getMatch() const {
+    switch (currentSearchCategory) {
+        case 1: return std::make_tuple(currentSearchText, QString(), QString());
+        case 2: return std::make_tuple(QString(), currentSearchText, QString());
+        case 3: return std::make_tuple(QString(), QString(), currentSearchText);
+        default: return std::make_tuple(QString(), QString(), QString());
+    }
+}
+
+void xPlayerMusicSearchWidget::clear() {
+    searchText->clear();
+    searchArtistButton->setChecked(true);
+    currentSearchCategory = -1;
+    currentSearchText.clear();
+}
+
+void xPlayerMusicSearchWidget::searchClicked() {
+    currentSearchText = searchText->text();
+    currentSearchCategory = searchCategoryGroup->checkedId();
+    if (currentSearchCategory != -1) {
+        emit updateFilter(getMatch());
+    } else {
+        clear();
+        emit clearFilter();
+    }
+}
+
+void xPlayerMusicSearchWidget::clearClicked() {
+    clear();
+    emit clearFilter();
+}
+
+void xPlayerMusicSearchWidget::returnPressed() {
+    currentSearchText = searchText->text();
+    currentSearchCategory = searchCategoryGroup->checkedId();
+    if (currentSearchText.isEmpty()) {
+        emit clearFilter();
+    } else {
+        searchClicked();
+    }
+}
---
+++ xPlayerMusicSearchWidget.h
@@ -0,0 +1,82 @@
+/*
+ * This file is part of xPlay.
+ *
+ * xPlay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __XPLAYERMUSICSEARCHWIDGET_H__
+#define __XPLAYERMUSICSEARCHWIDGET_H__
+
+#include <QButtonGroup>
+#include <QRadioButton>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QWidget>
+
+class xPlayerMusicSearchWidget:public QWidget {
+    Q_OBJECT
+
+public:
+    explicit xPlayerMusicSearchWidget(QWidget* parent=nullptr, Qt::WindowFlags flags=Qt::WindowFlags());
+    ~xPlayerMusicSearchWidget() override = default;
+    /**
+     * Retrieve the currently set match strings.
+     *
+     * @return a tuple of artist, album and track name match as string.
+     */
+    [[nodiscard]] std::tuple<QString,QString,QString> getMatch() const;
+    /**
+     * Cleanup.
+     *
+     * Reset search category to album and clear search text.
+     */
+    void clear();
+
+signals:
+    /**
+     * Signal emitted if clear button is clicked.
+     */
+    void clearFilter();
+    /**
+     * Signal emitted if search button is clicked.
+     *
+     * @param match the tuple of artist, album and track name match as string.
+     */
+    void updateFilter(const std::tuple<QString,QString,QString>& match);
+
+private slots:
+    /**
+     * Called if search button is clicked. Emit updateFilter signal.
+     */
+    void searchClicked();
+    /**
+     * Called if clear button is clicked. Clear and emit clearFilter signal.
+     */
+    void clearClicked();
+    /**
+     * Called if the return key is pressed in the search text input.
+     */
+    void returnPressed();
+
+private:
+    QButtonGroup* searchCategoryGroup;
+    QLineEdit* searchText;
+    QRadioButton* searchArtistButton;
+    QRadioButton* searchAlbumButton;
+    QRadioButton* searchTrackButton;
+    QPushButton* searchButton;
+    QPushButton* clearButton;
+    QString currentSearchText;
+    int currentSearchCategory;
+};
+
+
+#endif
