ddc1fdd91914522aa76407bba1e087e2606dcc63
Werner Backes <mail@wernerbackes.de>
2023-01-27 19:27:46 +0100

    Split rotel controls and widget.

---
+++ xPlayerRotelControls.cpp
@@ -0,0 +1,269 @@
+/*
+ * This file is part of xPlay.
+ *
+ * xPlay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#include "xPlayerRotelControls.h"
+#include "xPlayerConfiguration.h"
+
+#include <QList>
+#include <QTimer>
+
+xPlayerRotelControls* xPlayerRotelControls::rotelControls = nullptr;
+
+xPlayerRotelControls::xPlayerRotelControls():
+        QObject(),
+        rotelNetworkPort(0),
+        rotelNetworkAddress() {
+    // Create a socket to communicate with the amp.
+    rotelSocket = new QTcpSocket(this);
+    rotelSocket->setSocketOption(QTcpSocket::LowDelayOption, true);
+    rotelSocket->setSocketOption(QTcpSocket::KeepAliveOption, true);
+    QObject::connect(rotelSocket, &QTcpSocket::connected, this, &xPlayerRotelControls::controlsConnected);
+    QObject::connect(rotelSocket, &QTcpSocket::disconnected, this, &xPlayerRotelControls::controlsDisconnected);
+}
+
+xPlayerRotelControls* xPlayerRotelControls::controls() {
+    // Create and return singleton controls.
+    if (rotelControls == nullptr) {
+        rotelControls = new xPlayerRotelControls;
+    }
+    return rotelControls;
+}
+
+void xPlayerRotelControls::connect(const QString& address, int port, bool wait) {
+    // Do we have a valid address.
+    if ((address.isEmpty()) || (port <= 0)) {
+        qWarning() << "Rotel: not valid network address given: " << address << ":" << port;
+        return;
+    }
+    // Save current network address and port.
+    rotelNetworkPort = port;
+    rotelNetworkAddress = address;
+    // We do not connect if the widget is disabled.
+    if (!xPlayerConfiguration::configuration()->rotelWidget()) {
+        qInfo() << "Rotel: not connecting, widget is disabled.";
+        return;
+    }
+    rotelSocket->connectToHost(rotelNetworkAddress, rotelNetworkPort);
+    if (wait) {
+        rotelSocket->waitForConnected();
+    } else {
+        // Check after 60 seconds if the Rotel amp is online.
+        QTimer::singleShot(60000, this, &xPlayerRotelControls::controlsCheckConnection);
+    }
+}
+
+void xPlayerRotelControls::setVolume(int vol) {
+    auto adjVolume = std::clamp(vol, 0, Rotel::MaximumVolume);
+    auto volumeResponse = sendCommand(Rotel::SetVolume.arg(adjVolume, 2, 10, QChar('0')));
+    emit volume(adjVolume);
+    qDebug() << "RotelControls: setVolume: " << volumeResponse;
+}
+
+auto signString = [] (int value) {
+    if (value < 0) {
+        return QString("-");
+    } else if (value > 0) {
+        return QString("+");
+    } else {
+        return QString("0");
+    }
+};
+
+void xPlayerRotelControls::setBass(int b) {
+    auto adjBass = std::clamp(b, -10, 10);
+    auto bassResponse = sendCommand(Rotel::SetBass.arg(signString(adjBass)).arg(std::abs(adjBass), 2, 10, QChar('0')));
+    emit bass(adjBass);
+    qDebug() << "RotelControls: setBass: " << bassResponse;
+}
+
+void xPlayerRotelControls::setTreble(int t) {
+    auto adjTreble = std::clamp(t, -10, 10);
+    auto trebleResponse = sendCommand(Rotel::SetTreble.arg(signString(adjTreble)).arg(std::abs(adjTreble), 2, 10, QChar('0')));
+    emit treble(adjTreble);
+    qDebug() << "RotelControls: setTreble: " << trebleResponse;
+}
+
+void xPlayerRotelControls::setBalance(int b) {
+    auto adjBalance = std::clamp(b, -15, 15);
+    QString adjBalanceString("000");
+    // Lower-case 'l' and 'r' required even though document specified 'L' and 'R'.
+    if (adjBalance < 0) {
+        adjBalanceString = QString("l%1").arg(-adjBalance, 2, 10, QChar('0'));
+    } else if (adjBalance > 0) {
+        adjBalanceString = QString("r%1").arg(adjBalance, 2, 10, QChar('0'));
+    }
+    auto balanceResponse = sendCommand(Rotel::SetBalance.arg(adjBalanceString));
+    emit balance(adjBalance);
+    qDebug() << "RotelControls: setBalance: " << balanceResponse;
+}
+
+void xPlayerRotelControls::setSource(const QString& src) {
+    // Check for special "power off" command.
+    if (src == Rotel::Source_PowerOff) {
+        powerOff();
+        return;
+    }
+    auto srcIndex = Rotel::Sources.indexOf(src);
+    if (srcIndex >= 0) {
+        // The command for setting pc usb must be "pcusb" even though the return message is "pc_usb"
+        // Therefore we need to remove any "_" in the set source command.
+        auto srcResponse = sendCommand(Rotel::SetSource.arg(src).remove("_"));
+        emit source(src);
+        qDebug() << "RotelControls: setSource: " << srcResponse;
+    }
+}
+
+void xPlayerRotelControls::setMuted(bool m) {
+    auto muteResponse = sendCommand(Rotel::SetMute.arg(m ? "on" : "off"));
+    emit muted(m);
+    qDebug() << "RotelControls: setMute: " << muteResponse;
+}
+
+void xPlayerRotelControls::powerOff() {
+    auto powerOffResponse = sendCommand(Rotel::PowerOff);
+    emit disconnected();
+    qDebug() << "RotelControls: powerOff: " << powerOffResponse;
+}
+
+int xPlayerRotelControls::getVolume() {
+    QString volumeResponse = sendCommand(Rotel::RequestVolume);
+    qDebug() << "RotelControls: getVolume: " << volumeResponse;
+    if (!volumeResponse.isEmpty()) {
+        return volumeResponse.remove(Rotel::ResponsePrefixVolume).remove("$").toInt();
+    } else {
+        return -1;
+    }
+}
+
+int xPlayerRotelControls::getBass() {
+    QString bassResponse = sendCommand(Rotel::RequestBass);
+    qDebug() << "RotelControls: getBass: " << bassResponse;
+    if (!bassResponse.isEmpty()) {
+        return bassResponse.remove(Rotel::ResponsePrefixBass).remove("$").toInt();
+    } else {
+        return -1;
+    }
+}
+
+int xPlayerRotelControls::getTreble() {
+    QString trebleResponse = sendCommand(Rotel::RequestTreble);
+    qDebug() << "RotelControls: getTreble: " << trebleResponse;
+    if (!trebleResponse.isEmpty()) {
+        return trebleResponse.remove(Rotel::ResponsePrefixTreble).remove("$").toInt();
+    } else {
+        return -1;
+    }
+}
+
+int xPlayerRotelControls::getBalance() {
+    QString balanceResponse = sendCommand(Rotel::RequestBalance);
+    qDebug() << "RotelControls: getBalance: " << balanceResponse;
+    if (!balanceResponse.isEmpty()) {
+        return balanceResponse.remove(Rotel::ResponsePrefixBalance).remove("$").remove("R").replace("L","-").toInt();
+    } else {
+        return 0;
+    }
+}
+
+
+QString xPlayerRotelControls::getSource() {
+    auto srcString = sendCommand(Rotel::RequestSource);
+    qDebug() << "RotelControls: getSource: " << srcString;
+    if (!srcString.isEmpty()) {
+        return srcString.remove(Rotel::ResponsePrefixSource).remove("$");
+    } else {
+        return "";
+    }
+}
+
+bool xPlayerRotelControls::isMuted() {
+    auto muteString = sendCommand(Rotel::RequestMute);
+    qDebug() << "RotelControls: getMute: " << muteString;
+    if (!muteString.isEmpty()) {
+        return muteString.remove(Rotel::ResponsePrefixMute).startsWith("on");
+    } else {
+        return false;
+    }
+}
+
+QString xPlayerRotelControls::cleanupReplyMessage(const QString& message) {
+    qDebug() << "RotelControls: cleanupReplayMessage (before): " << message;
+    auto replyMessage = message;
+    while (replyMessage.startsWith("amp:freq")) {
+        replyMessage = replyMessage.remove(0, replyMessage.indexOf("$")+1);
+    }
+    qDebug() << "RotelControls: cleanupReplayMessage (after): " << replyMessage;
+    return replyMessage;
+}
+
+void xPlayerRotelControls::controlsConnected() {
+    // Update UI if connected.
+    emit volume(getVolume());
+    emit source(getSource());
+    emit muted(isMuted());
+    emit bass(getBass());
+    emit treble(getTreble());
+    emit balance(getBalance());
+    emit connected();
+}
+
+void xPlayerRotelControls::controlsDisconnected() {
+    // Update UI if disconnected.
+    rotelSocket->disconnectFromHost();
+    rotelSocket->abort();
+    emit disconnected();
+    // Check every 60 seconds if the Rotel amp is online.
+    QTimer::singleShot(60000, this, &xPlayerRotelControls::controlsCheckConnection);
+}
+
+void xPlayerRotelControls::controlsCheckConnection() {
+    // Return if we are already connected.
+    if (rotelSocket->state() == QTcpSocket::ConnectedState) {
+        return;
+    }
+    // Try to connect again.
+    rotelSocket->connectToHost(rotelNetworkAddress, rotelNetworkPort);
+    if (!rotelSocket->waitForConnected(1000)) {
+        // Still not connected? Then try again in 60 seconds.
+        QTimer::singleShot(60000, this, &xPlayerRotelControls::controlsCheckConnection);
+    }
+}
+
+QString xPlayerRotelControls::sendCommand(const QString& command) {
+    // We do ignore commands if the corresponding widget is disabled.
+    if (!xPlayerConfiguration::configuration()->rotelWidget()) {
+        qInfo() << "xPlayerRotelControls::sendcommand: ignore command, widget disabled.";
+        return {};
+    }
+    char readBuffer[65] = { 0 };
+    qint64 readBytes = 0;
+    rotelSocket->waitForConnected(500);
+    if (rotelSocket->state() == QTcpSocket::ConnectedState) {
+        rotelSocket->write(command.toStdString().c_str());
+        if (!rotelSocket->waitForBytesWritten(500)) {
+            qCritical() << "xPlayerRotelControls::sendCommand: unable to send command. Disconnecting.";
+            controlsDisconnected();
+            return {};
+        }
+        if (!rotelSocket->waitForReadyRead(2000)) {
+            qCritical() << "xPlayerRotelControls::sendCommand: unable to read reply. Disconnecting.";
+            controlsDisconnected();
+            return {};
+        }
+        readBytes = rotelSocket->read(readBuffer, 64);
+    } else {
+        qCritical() << "xPlayerRotelControls::sendCommand: state is not connected. Disconnecting.";
+        controlsDisconnected();
+    }
+    return (readBytes > 0) ? cleanupReplyMessage(QString(readBuffer)) : QString{};
+}
---
+++ xPlayerRotelControls.h
@@ -0,0 +1,257 @@
+/*
+ * This file is part of xPlay.
+ *
+ * xPlay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xPlay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __XPLAYERROTELCONTROLS_H__
+#define __XPLAYERROTELCONTROLS_H__
+
+#include <QTcpSocket>
+#include <QWidget>
+#include <QString>
+
+namespace Rotel {
+
+// Valid sources for the Rotel A14 or A12 amp
+const QList<QString> Sources { // NOLINT
+        "cd", "coax1", "coax2", "opt1", "opt2", "aux1", "aux2", "tuner", "phono", "usb", "bluetooth", "pc_usb"
+};
+// Special source indicating power off command.
+const QString Source_PowerOff = "power off"; // NOLINT
+// Limit the maximal volume.
+const int MaximumVolume = 60;
+// Command strings in order to communicate to and from the Rotel A14 or A12 amp.
+// Volume
+const QString RequestVolume = "amp:volume?"; // NOLINT
+const QString ResponsePrefixVolume = "amp:volume="; // NOLINT
+const QString SetVolume = "amp:vol_%1!"; // NOLINT
+// Bass
+const QString RequestBass = "amp:bass?"; // NOLINT
+const QString ResponsePrefixBass = "amp:bass="; // NOLINT
+const QString SetBass = "amp:bass_%1%2!"; // NOLINT
+// Volume
+const QString RequestTreble = "amp:treble?"; // NOLINT
+const QString ResponsePrefixTreble = "amp:treble="; // NOLINT
+const QString SetTreble = "amp:treble_%1%2!"; // NOLINT
+// Input source
+const QString RequestSource = "amp:source?"; // NOLINT
+const QString ResponsePrefixSource = "amp:source="; // NOLINT
+const QString SetSource = "amp:%1!"; // NOLINT
+// Mute
+const QString RequestMute = "amp:mute?"; // NOLINT
+const QString ResponsePrefixMute = "amp:mute="; // NOLINT
+const QString SetMute = "amp:mute_%1!"; // NOLINT
+// Mute
+const QString RequestBalance = "amp:balance?"; // NOLINT
+const QString ResponsePrefixBalance = "amp:balance="; // NOLINT
+const QString SetBalance = "amp:balance_%1!"; // NOLINT
+// Power off
+const QString PowerOff = "amp:power_off!"; // NOLINT
+
+}
+
+class xPlayerRotelControls:public QObject {
+    Q_OBJECT
+
+public:
+    /**
+     * Return the Rotel amp controls.
+     *
+     * @return pointer to a singleton of the rotel amp controls.
+     */
+    [[nodiscard]] static xPlayerRotelControls* controls();
+    /**
+     * Connect controls via network to the Rotel amp.
+     *
+     * @param address the address of the Rotel amp as string (IP or DNS name).
+     * @param port the port the controls connect to as integer.
+     * @param wait if true then wait until the socket is connected.
+     */
+    void connect(const QString& address, int port, bool wait=false);
+    /**
+     * Send get volume command to the Rotel amp and return retrieved volume.
+     *
+     * @return the current volume of the Rotel amp.
+     */
+    [[nodiscard]] int getVolume();
+    /**
+     * Send get bass command to the Rotel amp and return retrieved bass.
+     *
+     * @return the current bass of the Rotel amp.
+     */
+    [[nodiscard]] int getBass();
+    /**
+     * Send get treble command to the Rotel amp and return retrieved treble.
+     *
+     * @return the current treble of the Rotel amp.
+     */
+    [[nodiscard]] int getTreble();
+    /**
+     * Send get balance command to the Rotel amp and return retrieved treble.
+     *
+     * @return the current balance of the Rotel amp (< 0 to the left, > 0 to the right).
+     */
+    [[nodiscard]] int getBalance();
+    /**
+     * Send get source command to the Rotel amp and return retrieved source.
+     *
+     * @return the current source of the Rotel amp as string.
+     */
+    [[nodiscard]] QString getSource();
+    /**
+     * Send get mute command to the Rotel amp and return retrieved mute state.
+     *
+     * @return true if the Rotel amp is in mute state, false otherwise.
+     */
+    [[nodiscard]] bool isMuted();
+
+public slots:
+    /**
+     * Send set volume command to the Rotel amp.
+     *
+     * @param vol the new volume as integer.
+     */
+    void setVolume(int vol);
+    /**
+     * Send set bass command to the Rotel amp.
+     *
+     * @param b the new bass as integer.
+     */
+    void setBass(int b);
+    /**
+     * Send set treble command to the Rotel amp.
+     *
+     * @param t the new treble as integer.
+     */
+    void setTreble(int t);
+    /**
+     * Send set balance command to the Rotel amp.
+     *
+     * @param b the new balance as integer (< 0 to the left, > 0 to the right)
+     */
+    void setBalance(int b);
+    /**
+     * Send set source command to the Rotel amp.
+     *
+     * @param src the new source as string.
+     */
+    void setSource(const QString& src);
+    /**
+     * Send set mute on/off command to the Rotel amp.
+     *
+     * @param m enable mute if true, disable mute otherwise.
+     */
+    void setMuted(bool m);
+    /**
+     * Send command to power off the Rotel amp.
+     */
+    void powerOff();
+
+signals:
+    /**
+     * Signal emitted if controls are connected to the Rotel amp.
+     */
+    void connected();
+    /**
+     * Signal emitted if controls loose connection to the Rotel amp.
+     */
+    void disconnected();
+    /**
+     * Signal emitted if volume is updated by a setVolume command.
+     *
+     * @param vol the new volume as integer.
+     */
+    void volume(int vol);
+    /**
+     * Signal emitted if bass is updated by a setBass command.
+     *
+     * @param b the new volume as integer.
+     */
+    void bass(int b);
+    /**
+     * Signal emitted if treble is updated by a setTreble command.
+     *
+     * @param t the new volume as integer.
+     */
+    void treble(int t);
+    /**
+     * Signal emitted if balance is updated by a setBalance command.
+     *
+     * @param b the new balance as integer (< 0 to the left, > 0 to the right).
+     */
+    void balance(int b);
+    /**
+     * Signal emitted if source is updated by a setSource command.
+     *
+     * @param src the new source as string.
+     */
+    void source(const QString& src);
+    /**
+     * Signal emitted if the mute state is updated by a setMuted command.
+     *
+     * @param m the new mute state as boolean.
+     */
+    void muted(bool m);
+
+private slots:
+    /**
+     * Called if controls are connected to the Rotel amp.
+     *
+     * After the controls are connected, the initial state of the
+     * Rotel amp is retrieved and the corresponding signals are
+     * emitted.
+     */
+    void controlsConnected();
+    /**
+     * Called if controls are disconnected from the Rotel amp.
+     *
+     * The disconnect can also be called if one of the sendCommands
+     * to the Rotel amp fails. Try every 60 seconds to reconnect.
+     */
+    void controlsDisconnected();
+    /**
+     * Called every 60 seconds if Rotel amp is disconnected.
+     *
+     * Try to reconnect. If that fails try again in 60 seconds.
+     */
+    void controlsCheckConnection();
+    /**
+     * Cleanup any response commands that we do not support.
+     *
+     * @param message the raw reply message as string.
+     * @return the cleaned reply message as string.
+     */
+    static QString cleanupReplyMessage(const QString& message);
+
+private:
+    /**
+     * Constructor. Create socket and connect signals.
+     */
+    xPlayerRotelControls();
+    /**
+     * Destructor. Default.
+     */
+    ~xPlayerRotelControls() override = default;
+    /**
+     * Send command to the Rotel amp and retrieve the result.
+     *
+     * @param command the command send to the Rotel amp.
+     * @return the response send back by the Rotel amp.
+     */
+    QString sendCommand(const QString& command);
+
+    QTcpSocket* rotelSocket;
+    int rotelNetworkPort;
+    QString rotelNetworkAddress;
+    static xPlayerRotelControls* rotelControls;
+};
+
+#endif
